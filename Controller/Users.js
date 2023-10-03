62c16ce3485f4bf7b4213b04a5e7cd3a17722386
john ossai <johnossai20@gmail.com>
2023-10-03 04:52:19 -0700

    Quick Test

--- Controller/Users.js
+++ Controller/Users.js
@@ -76,7 +76,7 @@ const Login = async (req, res) => {
   }
 };
 
-const getUsers = async () => {
+const getUsers = async (req, res) => {
   try {
     const users = await User.find({});
     res.status(200).json({ users });
