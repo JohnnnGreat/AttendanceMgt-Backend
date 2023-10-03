c87fb2260350f13df4d1dccb2a0b48abdf2827eb
john ossai <johnossai20@gmail.com>
2023-10-03 00:50:26 -0700

    Made some changes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 //Port
 const PORT = process.env.PORT || 3030;
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Now Working, THank you" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server Running at Port ${PORT}`);
 });
